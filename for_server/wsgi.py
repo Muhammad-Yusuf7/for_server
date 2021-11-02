9de6763b9c06ea8d0e4e1487657c38ad3de86056
calculator <78301627+Muhammad-Yusuf7@users.noreply.github.com>
2021-11-02 16:07:06 +0300

    Update wsgi.py

--- for_server/wsgi.py
+++ for_server/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for OnlineApi project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
+# -*- coding: utf-8 -*-
 import os
+import sys
+import platform
+#путь к проекту, там где manage.py
+sys.path.insert(0, 'for_server/')
+#путь к фреймворку, там где settings.py
+sys.path.insert(0, 'for_server/for_server')
+#путь к виртуальному окружению myenv
+#sys.path.insert(0, '/home/c/cx53558/newsite/myenv/lib/python{0}/site-packages'.format(platform.python_version()[0:3]))
+#sys.path.insert(0, '/home/c/cx53558/newsite/myenv/lib/python3.6/site-packages')
+os.environ["DJANGO_SETTINGS_MODULE"] = "DjangoMySite.settings"
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineApi.settings')
-
 application = get_wsgi_application()
